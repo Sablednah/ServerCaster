97ea973b3535952952c59bfa5a3013740e556a92
Patrick <Patrick@zolder>
2014-02-08 22:57:14 +0100

    init

---
+++ src/me/killje/bukkit/BroadCaster.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package me.killje.bukkit;
+
+/**
+ *
+ * @author Patrick Beuks (s2288842), Floris Huizinga (s2397617) and
+ * @author Timo Smit (s2337789)
+ */
+public class BroadCaster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
