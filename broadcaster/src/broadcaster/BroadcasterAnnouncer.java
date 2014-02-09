16e9489a65d6d50e2c1d7ca7356a97be468c7901
Floris <Floris@Floris-PC>
2014-02-09 16:03:57 +0100

    Removed ; Fixed spelling of BotadcasterAnnouncer

--- broadcaster/src/broadcaster/BroadcasterAnnouncer.java
+++ broadcaster/src/broadcaster/BroadcasterAnnouncer.java
@@ -8,13 +8,13 @@ import mkremins.fanciful.FancyMessage;
  * @author Patrick Beuks (s2288842), Floris Huizinga (s2397617) and
  * @author Timo Smit (s2337789)
  */
-public class BroadcasterAnouncer implements Runnable {
+public class BroadcasterAnnouncer implements Runnable {
 
     private int lineIndex = 0;
     private final Broadcaster instance;
     private final BroadcastConverter converter = new BroadcastConverter();
 
-    BroadcasterAnouncer(Broadcaster aThis) {
+    BroadcasterAnnouncer(Broadcaster aThis) {
         instance = aThis;
         converter.populateList();
     }
@@ -28,7 +28,7 @@ public class BroadcasterAnouncer implements Runnable {
         String[] properMessages = converter.getProperMessage(messages.get(lineIndex));
         String prefix = instance.getConfig().getString("Prefix");
         if (!prefix.equals("")) {
-            prefix = prefix + ": ";
+            prefix = prefix + " ";
         }
         String test = new FancyMessage("").then("url").link(null).toJSONString();
         for (String message : properMessages) {
