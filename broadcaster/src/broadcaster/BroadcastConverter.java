d7a8a16b57a903793d4adfc900b9b5d9a9f15d72
Floris <Floris@Floris-PC>
2014-02-09 19:49:51 +0100

    saving

--- broadcaster/src/broadcaster/BroadcastConverter.java
+++ broadcaster/src/broadcaster/BroadcastConverter.java
@@ -2,6 +2,7 @@ package broadcaster;
 
 import java.util.HashMap;
 import java.util.Map;
+import mkremins.fanciful.FancyMessage;
 import org.bukkit.ChatColor;
 
 /**
@@ -11,67 +12,100 @@ import org.bukkit.ChatColor;
  */
 public class BroadcastConverter {
 
-    private Map<String, String> codeReplacement;
+    private Map<String, ChatColor> codeReplacement;
 
     public BroadcastConverter() {
     }
 
     public void populateList() {
         codeReplacement = new HashMap<>();
-        codeReplacement.put("&AQUA", ChatColor.AQUA.toString());
-        codeReplacement.put("&BLACK", ChatColor.BLACK.toString());
-        codeReplacement.put("&BLUE", ChatColor.BLUE.toString());
-        codeReplacement.put("&DARK_AQUA", ChatColor.DARK_AQUA.toString());
-        codeReplacement.put("&DARK_BLUE", ChatColor.DARK_BLUE.toString());
-        codeReplacement.put("&DARK_GRAY", ChatColor.DARK_GRAY.toString());
-        codeReplacement.put("&DARK_GREEN", ChatColor.DARK_GREEN.toString());
-        codeReplacement.put("&DARK_PURPLE", ChatColor.DARK_PURPLE.toString());
-        codeReplacement.put("&DARK_RED", ChatColor.DARK_RED.toString());
-        codeReplacement.put("&GOLD", ChatColor.GOLD.toString());
-        codeReplacement.put("&GRAY", ChatColor.GRAY.toString());
-        codeReplacement.put("&GREEN", ChatColor.GREEN.toString());
-        codeReplacement.put("&LIGHT_PURPLE", ChatColor.LIGHT_PURPLE.toString());
-        codeReplacement.put("&RED", ChatColor.RED.toString());
-        codeReplacement.put("&WHITE", ChatColor.WHITE.toString());
-        codeReplacement.put("&YELLOW", ChatColor.YELLOW.toString());
-        codeReplacement.put("&MAGIC", ChatColor.MAGIC.toString());
-        codeReplacement.put("&BOLD", ChatColor.BOLD.toString());
-        codeReplacement.put("&STRIKE", ChatColor.STRIKETHROUGH.toString());
-        codeReplacement.put("&UNDERLINE", ChatColor.UNDERLINE.toString());
-        codeReplacement.put("&ITALIC", ChatColor.ITALIC.toString());
-        codeReplacement.put("&RESET", ChatColor.RESET.toString());
+        codeReplacement.put("AQUA", ChatColor.AQUA);
+        codeReplacement.put("BLACK", ChatColor.BLACK);
+        codeReplacement.put("BLUE", ChatColor.BLUE);
+        codeReplacement.put("DARK_AQUA", ChatColor.DARK_AQUA);
+        codeReplacement.put("DARK_BLUE", ChatColor.DARK_BLUE);
+        codeReplacement.put("DARK_GRAY", ChatColor.DARK_GRAY);
+        codeReplacement.put("DARK_GREEN", ChatColor.DARK_GREEN);
+        codeReplacement.put("DARK_PURPLE", ChatColor.DARK_PURPLE);
+        codeReplacement.put("DARK_RED", ChatColor.DARK_RED);
+        codeReplacement.put("GOLD", ChatColor.GOLD);
+        codeReplacement.put("GRAY", ChatColor.GRAY);
+        codeReplacement.put("GREEN", ChatColor.GREEN);
+        codeReplacement.put("LIGHT_PURPLE", ChatColor.LIGHT_PURPLE);
+        codeReplacement.put("RED", ChatColor.RED);
+        codeReplacement.put("WHITE", ChatColor.WHITE);
+        codeReplacement.put("YELLOW", ChatColor.YELLOW);
+        codeReplacement.put("MAGIC", ChatColor.MAGIC);
+        codeReplacement.put("BOLD", ChatColor.BOLD);
+        codeReplacement.put("STRIKE", ChatColor.STRIKETHROUGH);
+        codeReplacement.put("UNDERLINE", ChatColor.UNDERLINE);
+        codeReplacement.put("ITALIC", ChatColor.ITALIC);
+        codeReplacement.put("RESET", ChatColor.RESET);
 
-        codeReplacement.put("&0", ChatColor.BLACK.toString());
-        codeReplacement.put("&1", ChatColor.DARK_BLUE.toString());
-        codeReplacement.put("&2", ChatColor.DARK_GREEN.toString());
-        codeReplacement.put("&3", ChatColor.DARK_AQUA.toString());
-        codeReplacement.put("&4", ChatColor.DARK_RED.toString());
-        codeReplacement.put("&5", ChatColor.DARK_PURPLE.toString());
-        codeReplacement.put("&6", ChatColor.GOLD.toString());
-        codeReplacement.put("&7", ChatColor.GRAY.toString());
-        codeReplacement.put("&8", ChatColor.DARK_GRAY.toString());
-        codeReplacement.put("&9", ChatColor.BLUE.toString());
-        codeReplacement.put("&a", ChatColor.GREEN.toString());
-        codeReplacement.put("&b", ChatColor.AQUA.toString());
-        codeReplacement.put("&c", ChatColor.RED.toString());
-        codeReplacement.put("&d", ChatColor.LIGHT_PURPLE.toString());
-        codeReplacement.put("&e", ChatColor.YELLOW.toString());
-        codeReplacement.put("&f", ChatColor.WHITE.toString());
-        codeReplacement.put("&k", ChatColor.MAGIC.toString());
-        codeReplacement.put("&l", ChatColor.BOLD.toString());
-        codeReplacement.put("&m", ChatColor.STRIKETHROUGH.toString());
-        codeReplacement.put("&n", ChatColor.UNDERLINE.toString());
-        codeReplacement.put("&o", ChatColor.ITALIC.toString());
-        codeReplacement.put("&r", ChatColor.RESET.toString());
+        codeReplacement.put("0", ChatColor.BLACK);
+        codeReplacement.put("1", ChatColor.DARK_BLUE);
+        codeReplacement.put("2", ChatColor.DARK_GREEN);
+        codeReplacement.put("3", ChatColor.DARK_AQUA);
+        codeReplacement.put("4", ChatColor.DARK_RED);
+        codeReplacement.put("5", ChatColor.DARK_PURPLE);
+        codeReplacement.put("6", ChatColor.GOLD);
+        codeReplacement.put("7", ChatColor.GRAY);
+        codeReplacement.put("8", ChatColor.DARK_GRAY);
+        codeReplacement.put("9", ChatColor.BLUE);
+        codeReplacement.put("a", ChatColor.GREEN);
+        codeReplacement.put("b", ChatColor.AQUA);
+        codeReplacement.put("c", ChatColor.RED);
+        codeReplacement.put("d", ChatColor.LIGHT_PURPLE);
+        codeReplacement.put("e", ChatColor.YELLOW);
+        codeReplacement.put("f", ChatColor.WHITE);
+        codeReplacement.put("k", ChatColor.MAGIC);
+        codeReplacement.put("l", ChatColor.BOLD);
+        codeReplacement.put("m", ChatColor.STRIKETHROUGH);
+        codeReplacement.put("n", ChatColor.UNDERLINE);
+        codeReplacement.put("o", ChatColor.ITALIC);
+        codeReplacement.put("r", ChatColor.RESET);
     }
 
     public String[] getProperMessage(String message) {
-        for (Map.Entry<String, String> entry : codeReplacement.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            message = message.replaceAll(key, value);
+        FancyMessage fm = new FancyMessage("");
+        int codeMessage = message.indexOf('&');
+        while (codeMessage != -1) {
+            fm.then(message.substring(0, codeMessage - 1));
+            int i = codeMessage + 1;
+            String code = "";
+            while (message.charAt(i)!=';') {
+                code += message.charAt(i);
+                i++;
+            }
+            for (Map.Entry<String, ChatColor> entry : codeReplacement.entrySet()) {
+                String string = entry.getKey();
+                ChatColor chatColor = entry.getValue();
+                if (string.equals(code)) {
+                    fm.then().color(chatColor);
+                }
+            }
+            codeMessage = message.indexOf('&');
         }
-        int index = 0;
+        int urlStart = message.indexOf("&URL");
+        while (urlStart != -1) {
+            int urlEnd = message.indexOf("&URLEND");
+            if (urlEnd != -1) {
+                int urlName = message.indexOf("&URLNAME");
+                String beforeUrl = message.substring(0, urlStart);
+                String afterUrl = message.substring(urlEnd + 7);
+                String urlText = "";
+                if (urlName != -1 && urlName < urlEnd) {
+                    String urlNameString = message.substring(urlName + 8, urlEnd);
+                    String urlString = message.substring(urlStart + 4, urlName);
+                    urlText = new FancyMessage(urlNameString).link(urlString).toJSONString();
+                } else {
+                    String urlString = message.substring(urlStart + 4, urlEnd);
+                    urlText = new FancyMessage(urlString).link(urlString).toJSONString();
+                }
+                message = beforeUrl + urlText + afterUrl;
+            }
+            urlStart = message.indexOf("&URL");
+        }
         return message.split("&NEW_LINE");
     }
 }
