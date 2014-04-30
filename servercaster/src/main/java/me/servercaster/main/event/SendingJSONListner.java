60133cfd5d5026289be0aeda19a4fab823defc47
killje <patrick.beuks@home.nl>
2014-04-30 13:11:28 +0200

    devided event in preServer and pre Player events

--- servercaster/src/main/java/me/servercaster/main/event/SendingJSONListner.java
+++ servercaster/src/main/java/me/servercaster/main/event/SendingJSONListner.java
@@ -5,5 +5,6 @@ package me.servercaster.main.event;
  * @author Patrick Beuks (killje) and Floris Huizinga (Flexo013)
  */
 public interface SendingJSONListner {
-    public void sendingJsonHandler(SendingJSONEvent e);
+    public void sendingPrePlayerHandler(PreSendingJSONToPlayerEvent e);
+    public void sendingPreServerHandler(PreSendingJSONToServerEvent e);
 }
