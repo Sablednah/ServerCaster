9c20efb64e02d3790ec034d4cf42902481ec6731
killje <patrick.beuks@home.nl>
2014-04-30 17:16:29 +0200

    added some javadoc for the events

--- servercaster/src/main/java/me/servercaster/main/event/PreSendingJSONToPlayerEvent.java
+++ servercaster/src/main/java/me/servercaster/main/event/PreSendingJSONToPlayerEvent.java
@@ -8,25 +8,47 @@ import org.bukkit.entity.Player;
  *
  * @author Patrick Beuks (killje) and Floris Huizinga (Flexo013)
  */
-public class PreSendingJSONToPlayerEvent extends EventObject{
-    
+public class PreSendingJSONToPlayerEvent extends EventObject {
+
     private ArrayList<String> messages;
     private final Player player;
 
+    /**
+     *
+     * @param messages The messages that are going to be send
+     * @param player the player that will receive the messages
+     * @param source source class of the event
+     */
     public PreSendingJSONToPlayerEvent(ArrayList<String> messages, Player player, Object source) {
         super(source);
         this.messages = messages;
         this.player = player;
     }
-    
+
+    /**
+     *
+     * @return the messages after they have been edited by
+     * PreSendingJSONToServerEvent note that with every fire of this event the
+     * messages are reset to the one just after PreSendingJSONToServerEvent
+     */
     public ArrayList<String> getMessages() {
         return messages;
     }
-    
-    public Player getPlayer(){
+
+    /**
+     *
+     * @return player that these messages are going to be send to
+     */
+    public Player getPlayer() {
         return player;
     }
 
+    /**
+     * If you have edited the messages send to the player you can set them with
+     * this function
+     *
+     * @param messages the new messages
+     */
     public void setMessages(ArrayList<String> messages) {
         this.messages = messages;
     }
--- servercaster/src/main/java/me/servercaster/main/event/PreSendingJSONToServerEvent.java
+++ servercaster/src/main/java/me/servercaster/main/event/PreSendingJSONToServerEvent.java
@@ -8,25 +8,46 @@ import org.bukkit.entity.Player;
  *
  * @author Patrick Beuks (killje) and Floris Huizinga (Flexo013)
  */
-public class PreSendingJSONToServerEvent extends EventObject{
-    
+public class PreSendingJSONToServerEvent extends EventObject {
+
     private ArrayList<String> messages;
     private final ArrayList<Player> players;
 
+    /**
+     *
+     * @param messages The messages that are going to be send
+     * @param players the players that will receive the messages
+     * @param source source class of the event
+     */
     public PreSendingJSONToServerEvent(ArrayList<String> messages, ArrayList<Player> players, Object source) {
         super(source);
         this.messages = messages;
         this.players = players;
     }
-    
+
+    /**
+     *
+     * @return the messages after they are converted. This event is fired once
+     * every broadcast.
+     */
     public ArrayList<String> getMessages() {
         return messages;
     }
     
-    public ArrayList<Player> getPlayers(){
+    /**
+     *
+     * @return players that these messages are going to receive
+     */
+    public ArrayList<Player> getPlayers() {
         return players;
     }
 
+    /**
+     * If you have edited the messages send to the players you can set them with
+     * this function again
+     *
+     * @param messages the new messages
+     */
     public void setMessages(ArrayList<String> messages) {
         this.messages = messages;
     }
