6292fad4fcbe3818f46066ad2ac54445777b3d83
killje <patrick.beuks@home.nl>
2014-04-30 16:23:36 +0200

    added players to sendServerEvent

--- servercaster/src/main/java/me/servercaster/main/event/PreSendingJSONToServerEvent.java
+++ servercaster/src/main/java/me/servercaster/main/event/PreSendingJSONToServerEvent.java
@@ -2,6 +2,7 @@ package me.servercaster.main.event;
 
 import java.util.ArrayList;
 import java.util.EventObject;
+import org.bukkit.entity.Player;
 
 /**
  *
@@ -10,14 +11,20 @@ import java.util.EventObject;
 public class PreSendingJSONToServerEvent extends EventObject{
     
     private ArrayList<String> messages;
+    private final ArrayList<Player> players;
 
-    public PreSendingJSONToServerEvent(ArrayList<String> messages, Object source) {
+    public PreSendingJSONToServerEvent(ArrayList<String> messages, ArrayList<Player> players, Object source) {
         super(source);
         this.messages = messages;
+        this.players = players;
     }
     
     public ArrayList<String> getMessages() {
         return messages;
+    }
+    
+    public ArrayList<Player> getPlayers(){
+        return players;
     }
 
     public void setMessages(ArrayList<String> messages) {
