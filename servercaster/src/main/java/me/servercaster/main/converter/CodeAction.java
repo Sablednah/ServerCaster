9535cbce0667079d14cdd4ef27041cab08130f75
Floris <Floris@Floris-PC>
2014-05-01 15:06:21 +0200

    Javadoc fixed

--- servercaster/src/main/java/me/servercaster/main/converter/CodeAction.java
+++ servercaster/src/main/java/me/servercaster/main/converter/CodeAction.java
@@ -3,7 +3,7 @@ package me.servercaster.main.converter;
 import mkremins.fanciful.FancyMessage;
 
 /**
- * This class can be used to create new Converters
+ * This class can be used to add actions when extended.
  *
  * @author Patrick Beuks (killje) and Floris Huizinga (Flexo013)
  */
@@ -15,8 +15,8 @@ public abstract class CodeAction {
 
     /**
      *
-     * @param arguments the amount of arguments this code needs (how many
-     * brackets it needs)
+     * @param arguments The amount of arguments this code needs (how many sets
+     * of brackets it needs)
      */
     public CodeAction(int arguments) {
         this.arguments = arguments;
@@ -25,15 +25,16 @@ public abstract class CodeAction {
 
     /**
      *
-     * @return String that represent the code word that needs to be found in the
-     * config.yml
+     * @return String that represents the code word that needs to be found in
+     * the config.yml
      */
     protected abstract String getKeyword();
 
     /**
-     * when a bracket is totally read doAction is called
+     * When the text within a pair of brackets is totally read, doAction is
+     * called
      *
-     * @param argument the text inside a bracket (is KeyWord found when
+     * @param argument The text inside a bracket (is KeyWord found when
      * arguments are 0)
      */
     public abstract void doAction(String argument);
@@ -69,34 +70,35 @@ public abstract class CodeAction {
 
     /**
      *
-     * @return the JSON lib handler
+     * @return The JSON lib handler
      */
     protected FancyMessage getJSONSaver() {
         return fm;
     }
 
     /**
-     * with this function you can set the amount of arguments used in this
-     * special code
+     * With this function you can set the amount of arguments used in this
+     * action code
      *
-     * @param arguments how many arguments this class should have
-     * @param reset if the remaining arguments should be reset to the given
+     * @param arguments How many arguments this action should have
+     * @param reset If the remaining arguments should be reset to the given
      * amount
      */
     protected void setArguments(int arguments, boolean reset) {
         this.arguments = arguments;
         if (reset) {
-            argumentsLeft = arguments;
+            if (!reset()) {
+                argumentsLeft = arguments;
+            }
         }
     }
 
     /**
-     * when a special code has no arguments left the class wont make a new
-     * instance. the number of arguments are automatically set back but if there
-     * are any more steps to take before its reset so that the program can
-     * continue you can specify them here.
+     * When an action has no arguments left the class won't make a new instance.
+     * The number of arguments are automatically set back, but if there are any
+     * more steps to take before it is reset, then you can specify them here.
      *
-     * @return return true if you don't want to reset argumentsLeft, default is
+     * @return Return true if you don't want to reset argumentsLeft, default is
      * false.
      */
     protected boolean reset() {
