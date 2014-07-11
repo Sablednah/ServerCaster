6ae7be02d9e66a53a4dfe34e7677f3353a040da4
killje <patrick.beuks@home.nl>
2014-07-11 17:44:41 +0200

    wirking with fanciful 2.0-SNAPSHOT after merge

---
+++ Groups/src/main/java/me/killje/servercaster/groups/Groups.java
@@ -0,0 +1,11 @@
+package me.killje.servercaster.groups;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Patrick Beuks (killje) and Floris Huizinga (Flexo013)
+ */
+public class Groups extends JavaPlugin{
+
+}
